9be9c9187803808e569aa6d8c1c4297f8e668e65
NIC0RE <106486232+NIC0RE@users.noreply.github.com>
2024-12-13 09:38:19 +0100

    All of TD1 - Roll multiple dices + logs

---
+++ dice/dice/src/main/java/miage/dice/DiceRollLogRepository.java
@@ -0,0 +1,7 @@
+package miage.dice;
+
+import miage.dice.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DiceRollLogRepository extends JpaRepository<DiceRollLog, Long> {
+}
